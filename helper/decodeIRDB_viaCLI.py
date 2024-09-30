e295b596ab8030e6d76941d5078401b1d8743e62
Benjamin Moeller <git@lupuse.org>
2024-09-30 22:30:18 +0200

    Comment helper

--- helper/decodeIRDB_viaCLI.py
+++ helper/decodeIRDB_viaCLI.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python3
+
+## This script is a helper to convert .ir files from the IRDB or
+## an own collection with 'parsed: raw' buttons, if the protocol
+## is known by the Flipper Zero.
+##
+## The analysis is done locally, so the local repo must contain
+## exact the same files as the Flipper Zero on the SD Card.
+##
+## Todo:
+## - Errorhandling, when no flipper attached
+## - status output. 1. collect files, 2 process files
+
 import os
 import serial
 import time
 
 IRDBonSystem = '/home/lupus/git/Flipper-IRDB/'
 IRDBonFlipper = '/ext/infrared/Flipper-IRDB/'
+ParsedFilesOnFlipper = '/ext/infrared/decoded/'
+FlipperZeroPort = '/dev/ttyACM0'
 
+## Filter files for only neccesary to convert
 def CheckForTypeRAW(irfile):
     with open(irfile) as f:
         if 'type: raw' in f.read():
             #print("true")
             return True
 
+## Send CLI command to Flipper Zero to parse files
 def GenerateDecodedFiles(IRPath, IRFile):
-	DecodedOnFlipper = '/ext/infrared/decoded/'
-	msg = 'ir decode '+IRDBonFlipper+IRPath+IRFile+' '+DecodedOnFlipper+IRPath+IRFile
+	msg = 'ir decode '+IRDBonFlipper+IRPath+IRFile+' '+ParsedFilesOnFlipper+IRPath+IRFile
 
 	print(msg)
 
-	ser = serial.Serial('/dev/ttyACM0')
+	ser = serial.Serial(FlipperZeroPort)
 	#ser.write(msg.encode(encoding = 'ascii', errors = 'strict')+bytes([13, 10]))
 	time.sleep(5)
 
+## Processing, walkthrough local filesystem -> give result to FZ CLI
+IRDBFileslist = []
 for subdir, dirs, files in os.walk(IRDBonSystem):
     for IRFile in files:
         if IRFile.endswith(".ir"):
             if CheckForTypeRAW(os.path.join(subdir, IRFile)):
                 #print(subdir + " and " + IRFile)
+                IRDBFileslist += subdir.replace(IRDBonSystem,""), IRFile
                 GenerateDecodedFiles(subdir.replace(IRDBonSystem,""), IRFile)
 
+print(len(IRDBFileslist)+" files processed.")
